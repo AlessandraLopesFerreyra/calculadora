b2f8f41ae57b47c39e3ba07bc32a46f72324ce83
AlessandraLopesFerreyra <159266652+AlessandraLopesFerreyra@users.noreply.github.com>
2024-09-02 09:39:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function calcula(opera){
+    let num1= parseFloat(document.getElementById(num).value);
+    let num2= parseFloat(document.getElementById(ndois).value);
+    let resultado= 0;
+    if(isNaN(num1) || isNaN(num2)){
+        alert('acorde pra vida.');
+    }else{
+         
+        if(opera == '+')
+    }
+}
